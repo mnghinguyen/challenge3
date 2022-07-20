a5c759724dabdccdf4f3ecf672db9075455a1547
Annie Nguyen <30813052+mnghinguyen@users.noreply.github.com>
2022-07-20 11:23:20 -0400

    Update script.js
    putting everything in child function

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
 // Assignment Code (get item in html by id)
 var generateBtn = document.querySelector("#generate");
 
-// Declare variables to generate random passwords
-
-// password length
-var passwordLength = [8, 12, 16];
-var characters = 'abcdefghijklmnopqrstuvwxyz0123456789@#$';
-var password = "";
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -20,11 +13,28 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+function generatePassword() {
+  // Declare variables to generate random passwords
+  // password length
+  var passwordLength = [8, 12, 16];
+  // password characters
+  var characters = 'abcdefghijklmnopqrstuvwxyz0123456789@#$';
+  // password
+  var password = "";
 
-// Ask user for their choice
-var passwordLength = function() {
-  var userChoice = window.prompt("Enter your password length (or number of characters) 8, 12 or 16");
-  if (!userChoice) {
-    return;
-  }
+for (var i = 0; i <= passwordLength; i++) {
+  var randomNumber = Math.floor(Math.random() * characters.length);
+  password += characters.substring(randomNumber, randomNumber +1);
+ }
+
+ // Associate password with input box
+document.getElementById("password").value = password;
+
 }
+// Ask user for their choice
+//var passwordLength = function() {
+//  var userChoice = window.prompt("Enter your password length (or number of characters) 8, 12 or 16");
+//  if (!userChoice) {
+//    return;
+// }
+//}
